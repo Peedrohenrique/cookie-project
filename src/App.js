bd0aab5375b19d100965ac41ccd3460001881369
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-11-06 16:54:54 -0300

    Início
    Estrutura base!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <di>
+        <h1>Hello World!</h1>
+      </di>
+    )
+  }
+}
+export default App;
